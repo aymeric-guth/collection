c1a5156ee859fed45160685386bcee03b278522a
yul <aymeric.guth@protonmail.com>
2023-01-17 20:59:47 +0100

    stack

--- main.go
+++ main.go
@@ -25,18 +25,12 @@ func regexWrapper(s string) *regexp.Regexp {
 
 func fileFinder(vault string) []string {
 	q := queue.New[string](vault)
-	// queue := make([]string, 0)
-	// queue = append(queue, vault)
-	// q.Push(vault)
 	ignorePath := regexWrapper(`(?:^.*imdone-tasks.*$)|(?:^\.)`)
 	ignoreLink := regexWrapper(`(?:^/400\sArchives.*$)|(?:^.*@Novall.*$)`)
 	result := make([]string, 0)
 
 	for q.Size() > 0 {
-		// for len(queue) > 0 {
 		path := q.Pop()
-		// path := queue[0]
-		// queue = queue[1:]
 		files, err := os.ReadDir(path)
 		if err != nil {
 			panic(err)
@@ -48,7 +42,6 @@ func fileFinder(vault string) []string {
 				continue
 			} else if file.IsDir() {
 				q.Push(filePath)
-				// queue = append(queue, filePath)
 			} else if filepath.Ext(file.Name()) == ".md" {
 				extension := filepath.Ext(file.Name())
 				result = append(result, relPath+"/"+file.Name()[:len(file.Name())-len(extension)]+filepath.Ext(file.Name()))
---
+++ stack/stack.go
@@ -0,0 +1,27 @@
+package stack
+
+type Stack[T any] struct {
+	d []T
+}
+
+func New[T any](args ...T) *Stack[T] {
+	return &Stack[T]{d: args}
+}
+
+func (s *Stack[T]) Push(v T) {
+	s.d = append(s.d, v)
+}
+
+func (s *Stack[T]) Pop() T {
+	v := s.d[len(s.d)-1]
+	s.d = s.d[:len(s.d)-1]
+	return v
+}
+
+func (s *Stack[T]) Peek() T {
+	return s.d[len(s.d)-1]
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.d)
+}
